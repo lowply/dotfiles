c2892f8444bbc349ae27487c72b59481672c3e06
Sho Mizutani <lowply@gmail.com>
2014-06-13 03:03:15 +0900

    wrote vim_installer, updated install_go and bashrc

--- go/install_go.sh
+++ go/install_go.sh
@@ -22,35 +22,36 @@ if [ -z "$GOINSTALLPATH" ]; then
 		ln -s /usr/local/go/bin/* .
 	;;
 	esac
-	
-	#
-	# gopath dir
-	#
-	if [ ! -d $HOME/go ]; then
-		mkdir $HOME/go
-	fi
-
-	#
-	# gocode
-	#
-	go get github.com/nsf/gocode
-	go get code.google.com/p/go.tools/cmd/godoc
-
-	#
-	# vim
-	#
-	cd $HOME/.vim/bundle/
-	case "${OSTYPE}" in
-	darwin*)
-		ln -Fis /usr/local/opt/go/libexec/misc/vim go
-	;;
-	linux*)
-		ln -Fis /usr/local/go/misc/vim go
-	;;
-	esac
-	ln -Fis $HOME/go/src/github.com/nsf/gocode/vim gocode
 else
 	echo "go is already installed"
 fi
 
 unset GOINSTALLPATH
+
+#
+# gopath dir
+#
+if [ ! -d $HOME/go ]; then
+	mkdir $HOME/go
+fi
+
+#
+# gocode
+#
+go get github.com/nsf/gocode
+go get code.google.com/p/go.tools/cmd/godoc
+
+#
+# vim
+#
+cd $HOME/.vim/bundle/
+case "${OSTYPE}" in
+darwin*)
+	ln -Fis /usr/local/opt/go/libexec/misc/vim go
+;;
+linux*)
+	ln -Fis /usr/local/go/misc/vim go
+;;
+esac
+ln -Fis $HOME/go/src/github.com/nsf/gocode/vim gocode
+
