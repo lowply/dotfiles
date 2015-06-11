464ef95481e917aefdea40bacd0e2dd5903fcefb
Sho Mizutani <lowply@gmail.com>
2015-06-11 10:33:56 +0900

    update pf22_stop.sh

--- bin/pf22_stop.sh
+++ bin/pf22_stop.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-/root/.pyenv/shims/supervisorctl stop pf22
+if [ -d ~/.pyenv ]; then
+	eval "$(~/.pyenv/bin/pyenv init -)"
+	SUPERVISOR=/root/.pyenv/shims/supervisorctl
+elif [ -x /usr/bin/supervisorctl ]; then
+	SUPERVISOR=/usr/bin/supervisorctl
+else
+	echo "supervisor is not installed"	
+	exit 1
+fi
+
+${SUPERVISOR} stop pf22
