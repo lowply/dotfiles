39257ac4efd2e3c0184235202ce5b6523329848b
Sho Mizutani <lowply@github.com>
2016-11-23 19:59:21 +1100

    Some updates

--- bin/install.sh
+++ bin/install.sh
@@ -59,14 +59,43 @@ create_backupdir(){
 	mkdir ${HOME}/dotfiles_backup_${DATE}
 }
 
-create_gitconfig_local(){
-	[ -z $(git config --get user.email) ] || { echo "user.email has already been configured"; return 0; }
-
-	local TARGET="${HOME}/.gitconfig.local"
+check_old_file(){
+	local TARGET=${1}
 	if [ -f ${TARGET} ]; then
 		mv ${TARGET} ${BACKUPDIR}
 		message warn "${TARGET} already exists, moving to ${BACKUPDIR}"
 	fi
+}
+
+create_bash_color(){
+	check_old_file "${HOME}/.bash_color"
+	cat <<- EOF > ${TARGET}
+		#
+		# bash color config
+		# type "psone" to take effect immediately
+		#
+
+		local BACKGROUND=0
+		local UNAME=198
+		local SYMBOL=147
+		local HOST=173
+		local DIRNAME=120
+		local PROMPT=129
+		local BRANCH=111
+	EOF
+	echo "${TARGET} is created"
+}
+
+create_bashrc_local(){
+	check_old_file "${HOME}/.bashrc.local"
+	cat <<- EOF > ${TARGET}
+		# env specific configs
+	EOF
+	echo "${TARGET} is created"
+}
+
+create_gitconfig_local(){
+	check_old_file "${HOME}/.gitconfig.local"
 
 	local EMAIL
 	echo "Type your email address for git and hit [ENTER]:"
@@ -80,39 +109,13 @@ create_gitconfig_local(){
 	echo "${TARGET} is created"
 }
 
-create_bash_color(){
-	local TARGET="${HOME}/.bash_color"
-	if [ ! -f ${TARGET} ]; then
-		cat <<- EOF > ${TARGET}
-			#
-			# bash color config
-			# type "psone" to take effect immediately
-			#
-
-			local BACKGROUND=0
-			local UNAME=198
-			local SYMBOL=147
-			local HOST=173
-			local DIRNAME=120
-			local PROMPT=129
-			local BRANCH=111
-		EOF
-		echo "${TARGET} is created"
-	else
-		echo "${TARGET} already exists"
-	fi
-}
-
 create_vimrc_local(){
-	local TARGET="${HOME}/.vimrc.local"
-	if [ ! -f ${TARGET} ]; then 
-		cat <<- EOF > ${TARGET}
-			" env specific configs
-		EOF
-		echo "${TARGET} is created"
-	else
-		echo "${TARGET} already exists"
-	fi
+	check_old_file "${HOME}/.vimrc.local"
+
+	cat <<- EOF > ${TARGET}
+		" env specific configs
+	EOF
+	echo "${TARGET} is created"
 }
 
 post_install(){
@@ -129,8 +132,9 @@ main(){
 		makedirs
 		link_init_nvim
 
-		create_gitconfig_local
 		create_bash_color
+		create_bashrc_local
+		create_gitconfig_local
 		create_vimrc_local
 
 		post_install
