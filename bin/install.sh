f8ac3ae9b04364f4a06a56dcef29f13b11630320
Sho Mizutani <lowply@github.com>
2017-07-29 02:07:47 +0900

    Fix a bug in install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -40,9 +40,8 @@ symlinks(){
 		if [ -L ${DST} ]; then
 			message info "A symlink ${DST} already exists, skipping"
 		elif [ -e ${DST} ]; then
-			message warn "A directory ${DST} already exists, moving to backup"
+			message warn "A file or a directory ${DST} already exists, moving to backup"
 			backup ${TARGET} ${DST}
-		else
 			ln -s ${SRC} ${DST}
 			message success "Created a symlink from ${SRC} to ${DST}"
 		fi
