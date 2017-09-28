bee9fa62a47c734081b41eb92f3b6aa1b9f15ff9
Sho Mizutani <lowply@github.com>
2017-09-28 19:09:32 +0900

    Remove pf22_stop

--- bin/pull_dotfiles.sh
+++ bin/pull_dotfiles.sh
@@ -2,6 +2,4 @@
 
 export PATH=/usr/local/bin:$PATH
 cd ~/dotfiles
-# not using submodules anymore
-# git pull -q origin master && git submodule -q foreach git pull -q origin master
 git pull -q origin master
