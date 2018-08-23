0d871855af22e66a115ceaf19298157fdf46a5d8
Sho Mizutani <lowply@github.com>
2018-08-24 02:03:48 +0900

    Fix a bug in letsencrypt.sh

--- bin/letsencrypt.sh
+++ bin/letsencrypt.sh
@@ -6,10 +6,10 @@ abort(){
 }
 
 usage(){
-	abort "Usage: ${0} example.com aws_profile <wc>"
+	abort "Usage: ${0} example.com aws_profile <yes|no> / the third parameter is for wildcard certificate"
 }
 
-[ $# -ne 2 ] && usage
+[ $# -ne 3 ] && usage
 [ -f ${HOME}/.aws/credentials ] || abort "aws credentials not found"
 [ -d ${HOME}/.acme.sh ] || abort "acme.sh not found"
 
@@ -19,12 +19,13 @@ WILDCARD=${3}
 AWS_ACCESS_KEY_ID="$(cat ~/.aws/credentials | grep "\[${PROFILE}\]" -A 2 | grep 'aws_secret_access_key' | sed -e 's/aws_secret_access_key = //')"
 AWS_SECRET_ACCESS_KEY="$(cat ~/.aws/credentials | grep "\[${PROFILE}\]" -A 2 | grep 'aws_access_key_id' | sed -e 's/aws_access_key_id = //')"
 
-if [ "${WILDCARD}" == "wc" ]; then
-	${HOME}/.acme.sh/acme.sh --issue --dns dns_aws \
-		-d *.${DOMAIN}
-		-d ${DOMAIN}
+if [ "${WILDCARD}" == "yes" ]; then
+	${HOME}/.acme.sh/acme.sh --issue \
+		-d ${DOMAIN} -d "*.${DOMAIN}" \
+		--dns dns_aws
 else
-	${HOME}/.acme.sh/acme.sh --issue --dns dns_aws \
-		-d ${DOMAIN}
+	${HOME}/.acme.sh/acme.sh --issue \
+		-d ${DOMAIN} \
+		--dns dns_aws
 fi
 
