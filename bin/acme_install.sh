fc6fe1c67ada4dc0b800af1a09f1eb3f35f70180
Sho Mizutani <lowply@github.com>
2019-04-11 04:34:50 +0900

    Update acme_install.sh

---
+++ bin/acme_install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+. $(dirname $0)/lib.sh
+
+install(){
+    local DOMAIN=${1}
+    local SSLPATH="/etc/nginx/ssl"
+    [ -d ${SSLPATH}/${DOMAIN} ] || mkdir ${SSLPATH}/${DOMAIN}
+
+    [ -f ${ACMEDIR}/${DOMAIN}/fullchain.cer ] || {
+        logger "Missing issued certificate"
+        return 1
+    }
+
+    [ -f ${SSLPATH}/${DOMAIN}/cert.pem ] && {
+        diff -q ${ACMEDIR}/${DOMAIN}/fullchain.cer ${SSLPATH}/${DOMAIN}/cert.pem > /dev/null 2>&1 && {
+            # Certificate isn't updated, silently skip the installation
+            return 0
+        }
+    }
+
+    logger "Installing certificate and reloading nginx..."
+
+    ${ACMEDIR}/acme.sh --install-cert -d ${DOMAIN} \
+        --key-file ${SSLPATH}/${DOMAIN}/key.pem \
+        --fullchain-file ${SSLPATH}/${DOMAIN}/cert.pem \
+        --reloadcmd "systemctl reload nginx"
+
+    if [ $? -eq 0 ]; then
+        message success "Installed certificate: ${DOMAIN}"
+        return 0
+    else
+        message error "Failed to install certificate: ${DOMAIN}"
+        return 1
+    fi
+}
+
+main(){
+    [ "$(whoami)" == "root" ] || abort "This command should be run by root."
+
+    ACMEDIR="${HOME}/.acme.sh"
+    check_dir "${ACMEDIR}"
+
+    CONFIGS=$(ls -1 /etc/nginx/conf.d/ | sed -e 's/\.conf//g' )
+
+    for DOMAIN in ${CONFIGS}
+    do
+        [ -d ${ACMEDIR}/${DOMAIN} ] || continue
+        logger "Started installation for ${DOMAIN}"
+        install ${DOMAIN} || {
+            exit 1
+        }
+    done
+
+    logger "Done installation"
+}
+
+main "$@"
