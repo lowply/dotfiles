5bf9c26efc0f1044e7df1931e352b1bd11f9637a
Sho Mizutani <lowply@github.com>
2022-09-28 00:12:47 +0900

    Update bin/connect-airpods.sh

--- bin/connect-airpods.sh
+++ bin/connect-airpods.sh
@@ -7,20 +7,26 @@ abort(){
     exit 1
 }
 
-[ -z "$AP_MAC" ] && abort "AP_MAC is missing" # XX-XX-XX-XX-XX-XX
-[ -z "$AP_NAME" ] && abort "AP_NAME is missing" # AirPods
-[ -z "$SOUND" ] && abort "SOUND is missing" # /path/to/sound.aiff
+# To see the UID, run:
+# SwitchAudioSource -a -f json
+[ -z "${AIRPODS_MAC}" ] && abort "AIRPODS_MAC is missing"
+[ -z "${SAS_UID_OUT}" ] && abort "SAS_UID_OUT is missing"
+[ -z "${SAS_UID_IN}" ] && abort "SAS_UID_IN is missing"
+
+SOUND=${SOUND:-"${HOME}/Dropbox/Sounds/Funk.aiff"}
 
 type BluetoothConnector > /dev/null 2>&1 || abort "BluetoothConnector not found. Run brew install bluetoothconnector"
 type SwitchAudioSource > /dev/null 2>&1 || abort "SwitchAudioSource not found. Run brew install switchaudio-osx"
 
-BluetoothConnector -c ${AP_MAC} || abort "Device not found"
+BluetoothConnector -c ${AIRPODS_MAC} || abort "Device not found"
 
 for ((i=0 ; i<10 ; i++)); do
     >&2 echo "Waiting for the device... ($i/10)"
-    if [ "Connected" == $(BluetoothConnector -s ${AP_MAC}) ]; then
-        SwitchAudioSource -s "${AP_NAME}" || abort "Failed to switch"
+    if [ "Connected" == $(BluetoothConnector -s ${AIRPODS_MAC}) ]; then
+        SwitchAudioSource -t input -u "${SAS_UID_IN}" || abort "Failed to switch"
+        SwitchAudioSource -t output -u "${SAS_UID_OUT}" || abort "Failed to switch"
         afplay "${SOUND}"
+        AP_NAME=$(SwitchAudioSource -a -f json | jq -r "select(.uid==\"${SAS_UID_OUT}\") | .name")
         >&2 echo "Connected and switched to ${AP_NAME}!"
         break
     fi
