67f74b6bdb00dccb4f4b863218000b1dc5736e4d
Sho Mizutani <lowply@github.com>
2022-12-14 23:31:43 +0900

    Discard post result

--- bin/keylight.sh
+++ bin/keylight.sh
@@ -4,4 +4,4 @@
 EP="http://${ELGATO_KEYLIGHT_IP}:9123/elgato/lights"
 CURRENT=$(curl -s ${EP})
 [ $(echo "${CURRENT}" | jq ".lights[0].on") == 0 ] && TARGET=1 || TARGET=0
-curl -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP}
+curl -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP} > /dev/null
