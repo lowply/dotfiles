d13e3edf972cffb4df80d23c540d76a2c387504b
Sho Mizutani <lowply@github.com>
2022-12-16 08:28:56 +0900

    Empty check

--- bin/keylight.sh
+++ bin/keylight.sh
@@ -3,5 +3,6 @@
 [ -z ${ELGATO_KEYLIGHT_IP} ] && { echo "ELGATO_KEYLIGHT_IP is empty"; exit 1; }
 EP="http://${ELGATO_KEYLIGHT_IP}:9123/elgato/lights"
 CURRENT=$(curl -s ${EP})
+[ -z ${CURRENT} ] && { echo "Empty output, perhaps failed to connect ${ELGATO_KEYLIGHT_IP}:9123?"; exit 1; }
 [ $(echo "${CURRENT}" | jq ".lights[0].on") == 0 ] && TARGET=1 || TARGET=0
 curl -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP} > /dev/null
