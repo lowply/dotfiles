7faef330f4f8745eaa9098b860b701285a988548
Sho Mizutani <lowply@github.com>
2023-06-21 23:52:13 +0900

    Handle a case when CURRENT_FILE is empty

--- bin/keylight.sh
+++ bin/keylight.sh
@@ -4,7 +4,8 @@
 [ -z ${ELGATO_KEYLIGHT_IP} ] && { echo "ELGATO_KEYLIGHT_IP is empty"; exit 1; }
 EP="http://${ELGATO_KEYLIGHT_IP}:9123/elgato/lights"
 CURRENT_FILE=${HOME}/.kl_current
-[ -f ${CURRENT_FILE} ] && CURRENT=$(cat ${CURRENT_FILE}) || CURRENT=$(curl -s ${EP})
+[ -f ${CURRENT_FILE} ] && CURRENT=$(cat ${CURRENT_FILE})
+[ -z ${CURRENT} ] && CURRENT=$(curl -s ${EP})
 [ -z ${CURRENT} ] && { echo "Empty output, perhaps failed to connect ${ELGATO_KEYLIGHT_IP}:9123?"; exit 1; }
 [ $(echo "${CURRENT}" | jq ".lights[0].on") == 0 ] && TARGET=1 || TARGET=0
 curl -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP} > ${CURRENT_FILE}
