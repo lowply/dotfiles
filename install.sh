2230b35816ad13d25dd663394e5ce285715a50af
Sho Mizutani <sho@svifa.net>
2024-08-21 19:03:21 -0700

    Revise the install script

--- install.sh
+++ install.sh
@@ -30,62 +30,59 @@ git-contrib(){
 }
 
 symlinks(){
-    cd ${WORKDIR}/symlinks
+    SRC="${WORKDIR}/symlinks"
 
-    local LIST_DIRS=$(find . -mindepth 1 -type d | sed -e 's/^\.\///') local LIST_FILES=$(find . -type f -not -name '.gitkeep' | sed -e 's/^\.\///')
+    find ${SRC} -type f | while IFS= read -r FILE; do
+        [ $(echo $FILE | xargs basename) = ".gitkeep" ] && continue
 
-    # Exclude .bashrc for Codespaces
-    if [ -n "$CODESPACES" ]; then
-        local LIST_FILES=$(echo ${LIST_FILES} | sed -e 's/.bashrc//')
-    fi
+        # Exclude .bashrc for Codespaces
+        [ -n "$CODESPACES" ] && [ $(echo $FILE | xargs basename) = ".bashrc" ] && continue
 
-    # make directories
-    for D in ${LIST_DIRS}; do
-        local DST="${HOME}/${D}"
-        [ -d ${DST} ] || mkdir -p ${DST}
-    done
+        DST="${FILE/${SRC}/${HOME}}"
+        [ -d $(dirname ${DST}) ] || mkdir -p $(dirname ${DST})
 
-    # make symlinks
-    for F in ${LIST_FILES}; do
-        local SRC="$(readlink -f ${F})"
-        local DST="${HOME}/${F}"
         if [ -L ${DST} ]; then
             message info "A symlink ${DST} already exists, skipping"
         elif [ -e ${DST} ]; then
             message warn "A file or a directory ${DST} already exists, moving to backup and create a symlink"
-            backup ${TARGET} ${DST}
-            ln -s ${SRC} ${DST}
+            backup ${DST}
+            ln -s ${FILE} ${DST}
         else
-            ln -s ${SRC} ${DST}
-            message success "Created a symlink from ${SRC} to ${DST}"
+            ln -s ${FILE} ${DST}
+            message success "Created a symlink from ${FILE} to ${DST}"
         fi
     done
 }
 
 copies(){
-    cd ${WORKDIR}/copies
-    local LIST_FILES=$(find . -type f -not -name '.gitkeep' | sed -e 's/^\.\///')
+    SRC="${WORKDIR}/copies"
 
-    for F in ${LIST_FILES}; do
-        local SRC="$(readlink -f ${F})"
-        local DST="${HOME}/${F}"
+    find ${SRC} -type f | while IFS= read -r FILE; do
+        [ $(echo $FILE | xargs basename) = ".gitkeep" ] && continue
+
+        DST="${FILE/${SRC}/${HOME}}"
+        [ -d $(dirname ${DST}) ] || mkdir -p $(dirname ${DST})
+
+        echo ${DST}
+
         if [ -L ${DST} ] || [ -e ${DST} ]; then
             message info "A file or a directory ${DST} already exists, skipping"
         else
-            cp ${SRC} ${DST}
-            message success "Copied file from ${SRC} to ${DST}"
+            cp ${FILE} ${DST}
+            message success "Copied file from ${FILE} to ${DST}"
         fi
     done
 }
 
 unlink(){
-    cd ${WORKDIR}/symlinks
-    local LIST_FILES=$(find . -type f -not -name '.gitkeep' | sed -e 's/^\.\///')
+    SRC="${WORKDIR}/symlinks"
 
-    for F in ${LIST_FILES}; do
-        local DST="${HOME}/${F}"
+    find ${SRC} -type f | while IFS= read -r FILE; do
+        [ $(echo $FILE | xargs basename) = ".gitkeep" ] && continue
+
+        DST="${FILE/${SRC}/${HOME}}"
+
         if [ -L ${DST} ]; then
-            rm -f ${DST}
             message success "${DST} has been unlinked"
         else
             message warn "${DST} isn't a symlink, doing nothing"
@@ -94,14 +91,14 @@ unlink(){
 }
 
 brew_bundle(){
-    cd ${WORKDIR}
     [[ ${OSTYPE} =~ ^darwin ]] && brew bundle || true
 }
 
 main(){
     [ $# -gt 1 ] && usage
 
-    WORKDIR=$(cd $(dirname $0); pwd)
+    cd $(dirname $0)
+    WORKDIR=$(pwd)
 
     if [[ ${OSTYPE} =~ ^darwin ]]; then
         export PATH="/opt/homebrew/bin:${PATH}"
@@ -109,14 +106,13 @@ main(){
         [ -L $(brew --prefix)/opt/coreutils ] || { echo "Install coreutils first."; exit 1; }
         export PATH="$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH"
     else
-        # Install Kitty terminfo and peco
-        sudo apt update && sudo apt install kitty-terminfo peco
-
-        # There's the Codespaces default .bashrc.
-        # Instead of overriding it, this adds my .bashrc at the end of the default .bashrc
-        [ -n "$CODESPACES" ] && \
-            echo ". ${WORKDIR}/symlinks/.bashrc" >> ${HOME}/.bashrc
+        # Install peco
+        has peco || { sudo apt update && sudo apt install peco; }
     fi
+
+    # There's the Codespaces default .bashrc.
+    # Instead of overriding it, this adds my .bashrc at the end of the default .bashrc
+    [ -n "$CODESPACES" ] && echo ". ${WORKDIR}/symlinks/.bashrc" >> ${HOME}/.bashrc
 
     case "${1}" in
         "clean")
