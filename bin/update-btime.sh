0c1eb5c8e8d2758bc91b30cd0a55432a8c8417ad
Sho Mizutani <sho@svifa.net>
2024-09-10 11:00:20 +0900

    Add usage and fix wrong file extension

--- bin/update-btime.sh
+++ bin/update-btime.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#/
+#/ Usage: update-btime.sh
+#/
+
 set -e
 
 . "$(dirname $0)/lib.sh"
@@ -9,7 +13,7 @@ has exiftool || abort "exiftool is not installed"
 # This isn't timezone compatible. The only way to modify the birth date
 # as I wish when outside of JST is to change the OS timezone to JST
 # before running this script.
-find . -type f -name "*.gz" | while IFS= read -r FILE; do
+find . -type f -name "*.jpg" | while IFS= read -r FILE; do
     CURRENT=$(/bin/date -r "$(/usr/bin/stat -f '%B' ${FILE})" "+%Y-%m-%d %H:%M:%S")
     DATE_CREATED_UNIX=$(exiftool -DateCreated "${FILE}" -d %s | cut -d ":" -f 2 | xargs)
     UPDATED=$(/bin/date -r "${DATE_CREATED_UNIX}" "+%Y-%m-%d %H:%M:%S")
