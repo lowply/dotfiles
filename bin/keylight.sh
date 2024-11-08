9c4b83724821b9fefd4cbe65231c9e0c780710db
Sho Mizutani <sho@svifa.net>
2024-11-09 00:22:53 +0900

    Add connection check and timeout

--- bin/keylight.sh
+++ bin/keylight.sh
@@ -4,8 +4,9 @@
 [ -z ${ELGATO_KEYLIGHT_IP} ] && { echo "ELGATO_KEYLIGHT_IP is empty"; exit 1; }
 EP="http://${ELGATO_KEYLIGHT_IP}:9123/elgato/lights"
 CURRENT_FILE=${HOME}/.kl_current
+ping -o -t 1 -q ${ELGATO_KEYLIGHT_IP} >/dev/null || { echo "${ELGATO_KEYLIGHT_IP} is not reachable"; exit 1; }
 [ -f ${CURRENT_FILE} ] && CURRENT=$(cat ${CURRENT_FILE})
-[ -z ${CURRENT} ] && CURRENT=$(curl -s ${EP})
+[ -z ${CURRENT} ] && CURRENT=$(curl -m 1 -s ${EP})
 [ -z ${CURRENT} ] && { echo "Empty output, perhaps failed to connect ${ELGATO_KEYLIGHT_IP}:9123?"; exit 1; }
 [ $(echo "${CURRENT}" | jq ".lights[0].on") == 0 ] && TARGET=1 || TARGET=0
-curl -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP} > ${CURRENT_FILE}
+curl -m 1 -s -X PUT -d "$(echo "${CURRENT}" | jq ".lights[0].on|=${TARGET}")" ${EP} > ${CURRENT_FILE}
