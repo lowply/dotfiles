0ca3ac0aea3a473dbe16225e30e3b9c972459c8e
Sho Mizutani <sho@svifa.net>
2024-11-11 09:57:47 +0900

    Indent with four spaces

--- bin/lib.sh
+++ bin/lib.sh
@@ -6,27 +6,27 @@ usage() {
 }
 
 logfile(){
-	LOGDIR="${HOME}/.log/$(basename $0)"
-	LOGFILE="${LOGDIR}/$(date +%y%m%d).log"
+    LOGDIR="${HOME}/.log/$(basename $0)"
+    LOGFILE="${LOGDIR}/$(date +%y%m%d).log"
 
-	mkdir -p "${LOGDIR}"
-	[ -f "${LOGFILE}" ] || touch ${LOGFILE}
-	echo "${LOGFILE}"
+    mkdir -p "${LOGDIR}"
+    [ -f "${LOGFILE}" ] || touch ${LOGFILE}
+    echo "${LOGFILE}"
 }
 
 logger(){
-	LOGFILE=$(logfile)
-	echo "$(date): [Info] ${1}" | tee -a "${LOGFILE}"
+    LOGFILE=$(logfile)
+    echo "$(date): [Info] ${1}" | tee -a "${LOGFILE}"
 }
 
 logger_error(){
-	LOGFILE=$(logfile)
-	echo "$(date): [Error] ${1}" | tee -a "${LOGFILE}" 1>&2
-	exit 1
+    LOGFILE=$(logfile)
+    echo "$(date): [Error] ${1}" | tee -a "${LOGFILE}" 1>&2
+    exit 1
 }
 
 abort(){
-	message error "${1}"; exit 1
+    message error "${1}"; exit 1
 }
 
 error(){
@@ -34,15 +34,15 @@ error(){
 }
 
 has(){
-	type "${1}" >/dev/null 2>&1; return $?
+    type "${1}" >/dev/null 2>&1; return $?
 }
 
 is_darwin(){
-	echo "${OSTYPE}" | grep -q "darwin"; return $?
+    echo "${OSTYPE}" | grep -q "darwin"; return $?
 }
 
 is_linux(){
-	echo "${OSTYPE}" | grep -q "linux"; return $?
+    echo "${OSTYPE}" | grep -q "linux"; return $?
 }
 
 is_ubuntu(){
@@ -50,61 +50,61 @@ is_ubuntu(){
 }
 
 is_codespaces(){
-	[ "${CODESPACES}" = "true" ]
+    [ "${CODESPACES}" = "true" ]
 }
 
 # ensure_* functions abort if the condition is not met
 
 ensure_args(){
-	[ "${1}" -eq "${2}" ] || abort "${3}"
+    [ "${1}" -eq "${2}" ] || abort "${3}"
 }
 
 ensure_dir(){
-	[ -d "${1}" ] || abort "No such directory: ${1}"
+    [ -d "${1}" ] || abort "No such directory: ${1}"
 }
 
 ensure_file(){
-	[ -f "${1}" ] || abort "No such file: ${1}"
+    [ -f "${1}" ] || abort "No such file: ${1}"
 }
 
 color(){
-	case "${1}" in
-		black) local COLOR="\e[30m" ;;
-		red) local COLOR="\e[31m" ;;
-		green) local COLOR="\e[32m" ;;
-		yellow) local COLOR="\e[33m" ;;
-		blue) local COLOR="\e[34m" ;;
-		magenta) local COLOR="\e[35m" ;;
-		cyan) local COLOR="\e[36m" ;;
-		white) local COLOR="\e[37m" ;;
-		*) local COLOR="\e[37m" ;;
-	esac
+    case "${1}" in
+        black) local COLOR="\e[30m" ;;
+        red) local COLOR="\e[31m" ;;
+        green) local COLOR="\e[32m" ;;
+        yellow) local COLOR="\e[33m" ;;
+        blue) local COLOR="\e[34m" ;;
+        magenta) local COLOR="\e[35m" ;;
+        cyan) local COLOR="\e[36m" ;;
+        white) local COLOR="\e[37m" ;;
+        *) local COLOR="\e[37m" ;;
+    esac
     local RESET="\e[39m"
-	printf "${COLOR}${2}${RESET}\n"
+    printf "${COLOR}${2}${RESET}\n"
 }
 
 message(){
-	case "${1}" in
-	success)
-		echo "[ $(color green OK) ] ${2}"
-	;;
-	info)
-		echo "[ $(color blue INFO) ] ${2}"
-	;;
-	user)
-		echo "[ $(color yellow ??) ] ${2}"
-	;;
-	warn)
-		echo "[ $(color yellow WARN) ] ${2}"
-	;;
-	error)
-		echo "[ $(color red ERROR) ] ${2}"
-	;;
-	fail)
-		echo "[ $(color red FAIL) ] ${2}"
-	;;
-	*)
-		echo "Incorrect Argument"
-	;;
-	esac
+    case "${1}" in
+    success)
+        echo "[ $(color green OK) ] ${2}"
+    ;;
+    info)
+        echo "[ $(color blue INFO) ] ${2}"
+    ;;
+    user)
+        echo "[ $(color yellow ??) ] ${2}"
+    ;;
+    warn)
+        echo "[ $(color yellow WARN) ] ${2}"
+    ;;
+    error)
+        echo "[ $(color red ERROR) ] ${2}"
+    ;;
+    fail)
+        echo "[ $(color red FAIL) ] ${2}"
+    ;;
+    *)
+        echo "Incorrect Argument"
+    ;;
+    esac
 }
